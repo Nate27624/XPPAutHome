922f9d8f88704cc070167c8a2f1eaaed7d1cd9a8
Nate27624 <arens.32@buckeyemail.osu.edu>
2026-04-27 22:58:06 -0400

    Set Vite base for GitHub Pages project deployment

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [react()],
+    base: "./",
     server: {
         port: 5173,
         host: true
